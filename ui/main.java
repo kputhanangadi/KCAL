f0a02e97dbc814e8be27aeb5b3b6e141a730bc7a
Kevin Puthanangadi <kputhanangadi@gmail.com>
2023-01-11 17:09:03 -0800

    Lab 1

---
+++ ui/main.java
@@ -0,0 +1,14 @@
+package ui;
+
+public class main {
+    public static void main(String[] args) {
+        methodname1();
+        methodname2();
+    }
+    public static void methodname1(){
+        System.out.println("Done1");
+    }
+    public static void methodname2(){
+        System.out.println("Done2");
+    }
+}

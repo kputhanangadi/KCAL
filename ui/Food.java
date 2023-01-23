8dc6414a99c70f975bfea4bdad7e371ebef400b8
Kevin Puthanangadi <kputhanangadi@gmail.com>
2023-01-22 21:09:48 -0800

    Changes by ryanyap

--- ui/Food.java
+++ ui/Food.java
@@ -2,13 +2,11 @@ package ui;
 
 public class Food {
     String name;
-    int calories;
 
     // CONSTRUCTOR
     public Food(String name) {
         // SETS FIELDS
         this.name = name;
-        this.calories = calories;
     }
 
 }

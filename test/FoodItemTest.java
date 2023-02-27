f1df433c90657412caa9866699ea6d04301236a6
Kevin Puthanangadi <kputhanangadi@gmail.com>
2023-02-26 21:07:57 -0800

    LAB 5

---
+++ test/FoodItemTest.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FoodItemTest {
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getCalorieCount() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
---
+++ test/JunkFoodTest.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JunkFoodTest {
+
+    @Test
+    void getType() {
+    }
+
+    @Test
+    void getCalorieCount() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
